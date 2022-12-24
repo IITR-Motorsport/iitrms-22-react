6c6119033b83a50b5e12421c5071e152d42c0017
--unset <nitesh_j@ee.iitr.ac.in>
2022-12-25 00:31:14 +0530

    check github connection

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-color": "#1E1E1E",
+        "green-color": "#A4CC3C",
+      },
+    },
+  },
+  plugins: [],
+};
