95b5a1f0f4c41eeeebf2fa3c89e098bc3c5eea86
niteshjangid29 <nitesh_j@ee.iitr.ac.in>
2023-02-21 12:43:29 +0530

    home page updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/about";
+import Team from "./pages/team";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <React.Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<Team />} />
+        </Routes>
+        <Footer />
+      </React.Fragment>
     </BrowserRouter>
   );
 }
